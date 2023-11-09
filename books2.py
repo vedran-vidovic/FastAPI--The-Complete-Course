dc2be4e5776e976181a33860da353af5f157137c
Vedran Vidovic <vedran.vidovic@ibmix.hr>
2023-11-09 14:09:29 +0100

    changed optional variable for pydantic v2

--- books2.py
+++ books2.py
@@ -25,7 +25,7 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = Field(title="ID is not needed")
+    id: Optional[int] = None
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
